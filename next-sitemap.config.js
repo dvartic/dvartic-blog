2b2d2d5eb10342f56eaa5b187ba689d5a5c1b249
dvartic <dvartic@users.noreply.github.com>
2022-08-07 10:52:01 +0200

    Sitemap generation config fix on URL

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://dvartic.xyz',
+    siteUrl: process.env.SITE_URL || 'https://www.dvartic.xyz',
     generateRobotsTxt: true, // (optional)
     generateIndexSitemap: false,
     // ...other options
