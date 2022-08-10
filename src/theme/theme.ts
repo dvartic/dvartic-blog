0d01a6bf4fde392865d60c07e82d4c6343c791a0
dvartic <dvartic@users.noreply.github.com>
2022-08-10 14:06:43 +0200

    Theme color update

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,14 @@
 import { extendTheme } from "@chakra-ui/react";
+import { mode } from '@chakra-ui/theme-tools';
 
 const theme = extendTheme({
+    styles: {
+        global: (props: any) => ({
+            body: {
+                bg: mode('gray.50', 'gray.800')(props)
+            }
+        })
+    },
     components: {
         Link: {
             variants: {
